7dd12d7e59d9ee2b5636be54fd34e7ef7521f812
Marek Oleksiak <marek@undefined.software>
2019-07-20 22:30:48 +0200

    basic setup for orcageddon

--- Assets/FishManager/Scripts/FishManager.cs
+++ Assets/FishManager/Scripts/FishManager.cs
@@ -19,11 +19,17 @@ namespace SGJ
         [SerializeField] private float MaxSpawnDelay = 1.0f;
         [SerializeField] private GameObject[] FishPrefabs;
 
+        [SerializeField] private GameObject[] OrcasPrefabs;
+        
+        [SerializeField] private float MinOrcasSpawnDelay = 10.5f;
+        [SerializeField] private float MaxOrcasSpawnDelay = 15.0f;
+
         void OnEnable()
         {
             WorldBounds = WorldRoot.CalculateBounds();
             IslandBounds = IslandRoot.CalculateBounds();
             StartCoroutine(SpawnFish());
+            StartCoroutine(SpawnOrca());
         }
 
         private void OnDisable()
@@ -63,30 +69,49 @@ namespace SGJ
                 yield return new WaitForSeconds(Random.Range(MinSpawnDelay, MaxSpawnDelay));
                 if (FishPrefabs.Length > 0)
                 {
-                    WorldBounds = WorldRoot.CalculateBounds();
-                    IslandBounds = IslandRoot.CalculateBounds();
-                    
                     var fish = Instantiate(FishPrefabs[Random.Range(0, FishPrefabs.Length)], GetRandomSource(), Quaternion.identity);
                     var rigid = fish.GetComponent<Rigidbody>();
-
-                    Vector3 p = GetRandomDestination();
-                    fish.transform.LookAt(p);
                     
-                    float angle = Random.Range(30f,70f) * Mathf.Deg2Rad;
- 
-                    Vector3 planarTarget = new Vector3(p.x, 0, p.z);
-                    Vector3 planarPostion = new Vector3(fish.transform.position.x, 0, fish.transform.position.z);
- 
-                    float distance = Vector3.Distance(planarTarget, planarPostion);
-                    float yOffset = fish.transform.position.y - p.y;
-                    float initialVelocity = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * Physics.gravity.magnitude * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
-                    Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
-                    float angleBetweenObjects = Vector3.Angle(Vector3.forward, planarTarget - planarPostion) * (p.x > fish.transform.position.x ? 1 : -1);
-                    Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
-                    
-                    rigid.AddForce(finalVelocity * rigid.mass, ForceMode.Impulse);
+                    ShootAt(rigid, GetRandomDestination());                    
                 }
             }
+        }
+
+        private IEnumerator SpawnOrca()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(MinSpawnDelay, MaxSpawnDelay));
+                if (OrcasPrefabs.Length > 0)
+                {
+                    var orca = Instantiate(OrcasPrefabs[Random.Range(0, OrcasPrefabs.Length)], GetRandomSource(), Quaternion.identity);
+                    var rigid = orca.GetComponent<Rigidbody>();
+                    
+                    ShootAt(rigid, GetRandomDestination());
+                }
+            }
+        }
+
+        private void ShootAt(Rigidbody projectile, Vector3 target)
+        {
+            WorldBounds = WorldRoot.CalculateBounds();
+            IslandBounds = IslandRoot.CalculateBounds();
+            
+            projectile.transform.LookAt(target);
+                    
+            float angle = Random.Range(30f,70f) * Mathf.Deg2Rad;
+ 
+            Vector3 planarTarget = new Vector3(target.x, 0, target.z);
+            Vector3 planarPostion = new Vector3(projectile.transform.position.x, 0, projectile.transform.position.z);
+ 
+            float distance = Vector3.Distance(planarTarget, planarPostion);
+            float yOffset = projectile.transform.position.y - target.y;
+            float initialVelocity = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * Physics.gravity.magnitude * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
+            Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
+            float angleBetweenObjects = Vector3.Angle(Vector3.forward, planarTarget - planarPostion) * (target.x > projectile.transform.position.x ? 1 : -1);
+            Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
+                    
+            projectile.AddForce(finalVelocity * projectile.mass, ForceMode.Impulse);
         }
 
 
